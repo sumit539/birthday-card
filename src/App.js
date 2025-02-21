9ba6dfd760dee0174b1853e53d277faf1b13b480
sumit539 <sumitsngh539@gmail.com>
2025-02-21 22:25:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,36 +9,36 @@
 //   );
 // }
 
-// import { useEffect } from "react";
-// import Card from "./card1";
-// import "./tulip1.css";
-// import { logPageView } from "./pageTracker"; // Import the tracking function
-
-// export default function App() {
-//     useEffect(() => {
-//         logPageView(); // Track visits silently in the background
-//     }, []);
-
-//     return (
-//         <div className="App">
-//             <Card />
-//         </div>
-//     );
-// }
-
 import { useEffect } from "react";
-import SunflowerCard from "./sunflowerCard"; // Import the new sunflower component
-import "./sunflower.css"; // Import the sunflower styles
-import { logPageView } from "./pageTracker"; // Tracking function
+import Card from "./card1";
+import "./tulip1.css";
+import { logPageView } from "./pageTracker"; // Import the tracking function
 
 export default function App() {
-  useEffect(() => {
-    logPageView(); // Track page view
-  }, []);
+    useEffect(() => {
+        logPageView(); // Track visits silently in the background
+    }, []);
 
-  return (
-    <div className="App">
-      <SunflowerCard />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Card />
+        </div>
+    );
 }
+
+// import { useEffect } from "react";
+// import SunflowerCard from "./sunflowerCard"; // Import the new sunflower component
+// import "./sunflower.css"; // Import the sunflower styles
+// import { logPageView } from "./pageTracker"; // Tracking function
+
+// export default function App() {
+//   useEffect(() => {
+//     logPageView(); // Track page view
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <SunflowerCard />
+//     </div>
+//   );
+// }
