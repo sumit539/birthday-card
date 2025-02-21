803903bb1d888379380bed68687023f9dc240b60
sumit539 <sumitsngh539@gmail.com>
2025-02-21 22:21:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,19 +9,36 @@
 //   );
 // }
 
+// import { useEffect } from "react";
+// import Card from "./card1";
+// import "./tulip1.css";
+// import { logPageView } from "./pageTracker"; // Import the tracking function
+
+// export default function App() {
+//     useEffect(() => {
+//         logPageView(); // Track visits silently in the background
+//     }, []);
+
+//     return (
+//         <div className="App">
+//             <Card />
+//         </div>
+//     );
+// }
+
 import { useEffect } from "react";
-import Card from "./card1";
-import "./tulip1.css";
-import { logPageView } from "./pageTracker"; // Import the tracking function
+import SunflowerCard from "./sunflowerCard"; // Import the new sunflower component
+import "./sunflower.css"; // Import the sunflower styles
+import { logPageView } from "./pageTracker"; // Tracking function
 
 export default function App() {
-    useEffect(() => {
-        logPageView(); // Track visits silently in the background
-    }, []);
+  useEffect(() => {
+    logPageView(); // Track page view
+  }, []);
 
-    return (
-        <div className="App">
-            <Card />
-        </div>
-    );
+  return (
+    <div className="App">
+      <SunflowerCard />
+    </div>
+  );
 }
